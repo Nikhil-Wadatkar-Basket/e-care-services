b1b0a73f8918a2a349ee24c16d8c6ef62c4ce1a7
Nikhil-Wadatkar-Basket <wadatkarnikhil22@gmail.com>
2023-03-18 22:35:33 +0530

    project added

---
+++ src/main/java/com/bs/ECareServicesApplication.java
@@ -0,0 +1,12 @@
+package com.bs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECareServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECareServicesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bs/ecareservices/ECareServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bs.ecareservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECareServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
